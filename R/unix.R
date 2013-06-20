25aa2ba8568b3f1747c245577bf00bc2cefa0cd6
Simon Urbanek <su@ix>
2013-06-20 00:24:36 -0400

    populate supplementary groups in set.user; add set.tempdir

--- R/unix.R
+++ R/unix.R
@@ -1,1 +1,2 @@
 set.user <- function(name, gid=NA) .Call(C_setuser, name, gid)
+set.tempdir <- function(path) invisible(.Call(C_setTempDir, path))
--- src/unix.c
+++ src/unix.c
@@ -15,8 +15,11 @@ SEXP C_setuser(SEXP sName, SEXP sGID) {
 	    Rf_error("user '%s' not found", CHAR(STRING_ELT(sName, 0)));
 	if (gid >= 0 && setgid(gid))
 	    Rf_error("failed to set gid (%d)", gid);
-	else if (setgid(p->pw_gid))
-	    Rf_error("failed to set gid (%d) from passwd for user %s", p->pw_gid, CHAR(STRING_ELT(sName, 0)));
+	else {
+	    if (setgid(p->pw_gid))
+		Rf_error("failed to set gid (%d) from passwd for user %s", p->pw_gid, CHAR(STRING_ELT(sName, 0)));
+	    initgroups(p->pw_name, p->pw_gid);
+	}
 	if (setuid(p->pw_uid))
 	    Rf_error("failed to set uid (%d) from passwd for user %s", p->pw_uid, CHAR(STRING_ELT(sName, 0)));
 	return ScalarInteger(p->pw_uid);
@@ -26,4 +29,13 @@ SEXP C_setuser(SEXP sName, SEXP sGID) {
 	if (uid >= 0 && setuid(uid)) Rf_error("failed to set uid (%d)", uid);
     }
     return ScalarLogical(1);
+}
+
+#include <Rembedded.h>
+
+SEXP C_setTempDir(SEXP sName) {
+    if (TYPEOF(sName) != STRSXP || LENGTH(sName) != 1)
+	Rf_error("invalid path");
+    R_TempDir = strdup(CHAR(STRING_ELT(sName, 0)));
+    return sName;
 }
