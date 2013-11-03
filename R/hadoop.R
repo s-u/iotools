ffdd06e51a410ff229c5bbcce5f8c4730b113c21
Simon Urbanek <simon.urbanek@r-project.org>
2013-11-02 22:18:44 -0400

    add reducers option to hmr()

--- R/hadoop.R
+++ R/hadoop.R
@@ -5,7 +5,7 @@ hpath <- function(path) structure(path, class="HDFSpath")
 hinput <- function(path, formatter=function(x) { y <- mstrsplit(x, '|', '\t'); if (ncol(y) == 1L) y[,1] else y })
   structure(path, class=c("hinput", "HDFSpath"), formatter=formatter)
 
-hmr <- function(input, output, map=identity, reduce=identity, job.name, aux, formatter, packages=loadedNamespaces()) {
+hmr <- function(input, output, map=identity, reduce=identity, job.name, aux, formatter, packages=loadedNamespaces(), reducers) {
   .rn <- function(n) paste(sprintf("%04x", as.integer(runif(n, 0, 65536))), collapse='')
   if (missing(output)) output <- hpath(sprintf("/tmp/io-hmr-temp-%d-%s", Sys.getpid(), .rn(4)))
   if (missing(job.name)) job.name <- sprintf("RCloud:iotools:hmr-%s", .rn(2))
@@ -36,10 +36,11 @@ hmr <- function(input, output, map=identity, reduce=identity, job.name, aux, for
   save(list=ls(envir=e, all.names=TRUE), envir=e, file="stream.RData")
   map.cmd <- if (identical(map, identity)) "" else "-mapper \"R --slave --vanilla -e 'iotools:::run.map()'\""
   reduce.cmd <- if (identical(reduce, identity)) "" else "-reducer \"R --slave --vanilla -e 'iotools:::run.reduce()'\""
+  extraD <- if (missing(reducers)) "" else paste0("-D mapred.reduce.tasks=", as.integer(reducers))
   system(paste(
                shQuote(hcmd), "jar", shQuote(sj),
                "-D", "mapreduce.reduce.input.limit=-1",
-               "-D", shQuote(paste0("mapred.job.name=", job.name)),
+               "-D", shQuote(paste0("mapred.job.name=", job.name)), extraD,
                "-input", shQuote(input),
                "-output", shQuote(output),
                "-file", "stream.RData",
