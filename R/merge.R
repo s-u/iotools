ab4a8987415e3ea7987fa31e65570d141eadc791
Simon Urbanek <simon.urbanek@r-project.org>
2014-02-07 09:29:17 -0500

    add auto-close option to line.merge (on by default)

--- R/merge.R
+++ R/merge.R
@@ -1,9 +1,11 @@
 which.min.key <- function(keys, sep="|") .Call(which_min_key, keys, sep)
 
-line.merge <- function(sources, target, sep='|') {
+line.merge <- function(sources, target, sep='|', close=TRUE) {
     l <- lapply(sources, readLines, 1L)
     while(length(i <- which.min.key(l, sep))) {
         writeLines(l[[i]], target)
 	l[[i]] <- readLines(sources[[i]], 1L)
     }
+    if (isTRUE(close))
+        for(c in sources) close(c)
 }
