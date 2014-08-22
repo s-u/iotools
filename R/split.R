275b2bee15a49b65f749bc62f472a049895a874a
Taylor Arnold <taylor.b.arnold@gmail.com>
2014-08-22 11:36:58 -0400

    add type argument to R function

--- R/split.R
+++ R/split.R
@@ -1,1 +1,5 @@
-mstrsplit <- function(x, sep="|", nsep=NA, line=1L, quiet=FALSE, ncol = NA) .Call(mat_split, x, sep, nsep, line, quiet, ncol)
+mstrsplit <- function(x, sep="|", nsep=NA, line=1L, quiet=FALSE, ncol = NA,
+                      type=c("character", "numeric")) {
+  type_flag = as.integer(match.arg(type) == "character")
+  .Call(mat_split, x, sep, nsep, line, quiet, ncol, type_flag)
+}
