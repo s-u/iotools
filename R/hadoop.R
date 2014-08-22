eebd777788d2e0cf9241ca46a462d2b05de9607f
Taylor Arnold <taylor@taylors-mbp.client.research.att.com>
2014-08-22 11:26:20 -0400

    adding c.hinput code to combine hinput files

--- R/hadoop.R
+++ R/hadoop.R
@@ -9,6 +9,20 @@ hpath <- function(path) structure(path, class="HDFSpath")
 hinput <- function(path, formatter=function(x) { y <- mstrsplit(x, '|', '\t'); if (ncol(y) == 1L) y[,1] else y })
   structure(path, class=c("hinput", "HDFSpath"), formatter=formatter)
 
+c.hinput = function(..., recursive = FALSE) {
+  if(!all(sapply(list(...), function(v) all.equal(attr(v, "class"), c("hinput", "HDFSpath"))) == TRUE)) {
+    warning("Using default combine function as not all objects are of class hinput")
+    return(c(sapply(list(...), function(v) as.character(v))))
+  }
+  formatters = sapply(list(...), function(v) attr(v, "formatter"))
+  if(!all(sapply(formatters, all.equal, formatters[[1]]))) {
+    stop("All input objects must have the same formatters")
+  }
+
+  return(structure(unique(c(sapply(list(...), function(v) as.character(v)))),
+         class=c("hinput", "HDFSpath"), formatter=formatters[[1]]))
+}
+
 .hadoop.detect <- function() {
   hh <- Sys.getenv("HADOOP_HOME")
   if (!nzchar(hh)) hh <- Sys.getenv("HADOOP_PREFIX")
