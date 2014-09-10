fdcac433330930d7db7a392d6838dd51fec49878
Taylor Arnold <taylor.b.arnold@gmail.com>
2014-09-10 11:52:42 -0400

    changing mstrsplit option quiet to strict

--- R/split.R
+++ R/split.R
@@ -1,5 +1,5 @@
-mstrsplit <- function(x, sep="|", nsep=NA, line=1L, quiet=FALSE, ncol = NA,
+mstrsplit <- function(x, sep="|", nsep=NA, line=1L, strict=TRUE, ncol = NA,
                       type=c("character", "numeric")) {
   type_flag = as.integer(match.arg(type) == "character")
-  .Call(mat_split, x, sep, nsep, line, quiet, ncol, type_flag)
+  .Call(mat_split, x, sep, nsep, line, !strict, ncol, type_flag)
 }
