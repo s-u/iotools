ed4962c69c1e84be0f5b7447f6b9adf869cddb1b
Taylor Arnold <taylor@Taylors-MacBook-Pro.local>
2014-11-24 18:32:29 +0000

    removing run.chunk for local.R

--- R/local.R
+++ R/local.R
@@ -1,12 +1,13 @@
-# wrappers for using iotools locally; loads input, writes output, and
-# operates on a file on the disk by localling walking through chunks
-# of data and writing to disk; the latter somewhat replicates the
-# chunk.apply functions and may eventually be removed
+# wrappers for using iotools locally; loads input, writes output,
 
 input <- function(file, formatter = .default.formatter, max.line = 65536L) {
   if (is.character(file)) {
     input = file(file, "rb")
     on.exit(close(input))
+  } else if (is.connection(file)) {
+    input = file
+  } else {
+    stop("'file' must be a connection or a character string to a file path.")
   }
   reader = chunk.reader(input, max.line=max.line)
   output = NULL
@@ -22,29 +23,13 @@ output <- function(x, file, formatter.output = NULL) {
   if (is.character(ret <- file)) {
     output = file(file, "wb")
     on.exit(close(output))
+  } else if (is.connection(file)) {
+    output = file
+  } else {
+    stop("'file' must be a connection or a character string to a file path.")
   }
   if (is.null(formatter.output)) formatter.output <- as.output
 
   writeLines(formatter.output(x), output)
-  invisible(ret)
-}
-
-run.block = function(input, output = tempfile(), FUN = identity,
-                  formatter=function(m) mstrsplit(m, "|")) {
-  if (is.character(input)) {
-    input = file(input, "rb")
-    on.exit(close(input))
-  }
-  if (is.character(ret <- output)) {
-    output = file(output, "wb")
-    on.exit(close(output))
-  }
-
-  reader = chunk.reader(input)
-  while (TRUE) {
-    chunk = read.chunk(reader)
-    if (!length(chunk)) break
-    writeLines(as.output(FUN(formatter(chunk))), output)
-  }
   invisible(ret)
 }
