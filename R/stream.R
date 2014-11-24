d4a31123ca6aff5f680d940130baeb1496550295
Taylor Arnold <taylor@Taylors-MacBook-Pro.local>
2014-11-24 18:51:51 +0000

    exporting as.output and added docs

--- R/stream.R
+++ R/stream.R
@@ -4,12 +4,12 @@ ioreadBlock <- function(input, block=100000L) {
 iostream <- function(expr, sep="|", input=file("stdin"), output=file("stdout"), block=100000L) {
 }
 
-as.output <- function(x) UseMethod("as.output")
-as.output.default <- function(x) if (is.null(names(x))) as.character(x) else paste(names(x), as.character(x), sep='\t')
-as.output.table <- function(x) paste(names(x), x, sep='\t')
-as.output.matrix <- function(x) { o <- apply(x, 1, paste, collapse='|'); if (!is.null(rownames(x))) o <- paste(rownames(x), o, sep='\t'); o }
-as.output.list <- function(x) paste(names(x), sapply(x, function (e) paste(as.character(e), collapse='|')), sep='\t')
-as.output.data.frame <- function(x) { if (ncol(x) == 1L) return(as.character(x[,1])); o <- apply(x[,-1,drop=FALSE], 1, paste, collapse='|'); o <- paste(x[,1], o, sep='\t'); o }
+as.output <- function(x, ...) UseMethod("as.output")
+as.output.default <- function(x, ...) if (is.null(names(x))) as.character(x) else paste(names(x), as.character(x), sep='\t')
+as.output.table <- function(x, ...) paste(names(x), x, sep='\t')
+as.output.matrix <- function(x, ...) { o <- apply(x, 1, paste, collapse='|'); if (!is.null(rownames(x))) o <- paste(rownames(x), o, sep='\t'); o }
+as.output.list <- function(x, ...) paste(names(x), sapply(x, function (e) paste(as.character(e), collapse='|')), sep='\t')
+as.output.data.frame <- function(x, ...) { if (ncol(x) == 1L) return(as.character(x[,1])); o <- apply(x[,-1,drop=FALSE], 1, paste, collapse='|'); o <- paste(x[,1], o, sep='\t'); o }
 
 ## this is almost like run.chunked() except for passing it to the worker
 run.persistent <- function(FUN, formatter=mstrsplit, key.sep=NULL) {
