fb1aca666d1ccd6a25d2ef5156e6936d4a0127b9
Taylor Arnold <taylor.b.arnold@gmail.com>
2014-12-17 13:57:30 +0000

    fixing spurious is.connection call

--- R/local.R
+++ R/local.R
@@ -4,7 +4,7 @@ input <- function(file, formatter = .default.formatter, max.line = 65536L) {
   if (is.character(file)) {
     input = file(file, "rb")
     on.exit(close(input))
-  } else if (inherits(f, "connection")) {
+  } else if (inherits(file, "connection")) {
     input = file
   } else {
     stop("'file' must be a connection or a character string to a file path.")
@@ -23,7 +23,7 @@ output <- function(x, file, formatter.output = NULL) {
   if (is.character(ret <- file)) {
     output = file(file, "wb")
     on.exit(close(output))
-  } else if (is.connection(file)) {
+  } else if (inherits(file, "connection")) {
     output = file
   } else {
     stop("'file' must be a connection or a character string to a file path.")
