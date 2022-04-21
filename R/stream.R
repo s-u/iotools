cec1f4f043a599763dbbe6e3e375a6b28599fd5d
Simon Urbanek <simon.urbanek@r-project.org>
2022-04-20 22:44:01 -0400

    chunk.apply: honor CH.MAX.SIZE in parallel jobs

--- R/stream.R
+++ R/stream.R
@@ -14,7 +14,7 @@ chunk.apply <- function(input, FUN, ..., CH.MERGE=rbind, CH.MAX.SIZE=33554432,
     worker_queue = list()
     # Fill the worker queue.
     for (i in 1:max(parallel, 1)) {
-      chunk = read.chunk(reader)
+      chunk = read.chunk(reader, max.size=CH.MAX.SIZE)
       if (length(chunk) == 0) {
         break
       }
@@ -23,7 +23,7 @@ chunk.apply <- function(input, FUN, ..., CH.MERGE=rbind, CH.MAX.SIZE=33554432,
     if (length(worker_queue) == 0) return(CH.MERGE(NULL))
 
     # Pre-fetch the next chunk if we not at the end of input.
-    if (length(chunk) > 0) chunk = read.chunk(reader)
+    if (length(chunk) > 0) chunk = read.chunk(reader, max.size=CH.MAX.SIZE)
 
     # Process the chunk-stream.
     ret=NULL
@@ -33,7 +33,7 @@ chunk.apply <- function(input, FUN, ..., CH.MERGE=rbind, CH.MAX.SIZE=33554432,
       worker_queue[1] = NULL
       if (length(chunk) > 0) {
         worker_queue[[length(worker_queue)+1]] = parallel::mcparallel(FUN(chunk, ...))
-        chunk = read.chunk(reader)
+        chunk = read.chunk(reader, max.size=CH.MAX.SIZE)
       }
       if (length(worker_queue) == 0) done = TRUE
     }
